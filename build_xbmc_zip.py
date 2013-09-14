0a88fc2951c14385c6ed467240f320e05fc92f2a
Andy Botting <andy@andybotting.com>
2013-09-14 14:36:57 +1000

    Only build _deps zip file if deps directory exists

--- build_xbmc_zip.py
+++ build_xbmc_zip.py
@@ -42,31 +42,33 @@ for r, d, f in os.walk('.'):
       z.write(os.path.join(r, ff), os.path.join(name, r, ff), zipfile.ZIP_DEFLATED)
 z.close()
 
-# Build XBMC ZIP file with all required depenencies
-zfilename = "%s-%s_deps.zip" % (name, version)
+# Build XBMC ZIP file with all required depenencies if deps
+# directory exists
+if os.path.isdir(os.path.join('resources','lib','deps')):
+  zfilename = "%s-%s_deps.zip" % (name, version)
 
-# Walk the directory to create the zip file
-print("Writing ZIP file: %s" % zfilename)
-z = zipfile.ZipFile(zfilename, 'w')
-for r, d, f in os.walk('.'):
-  for ff in f:
-    skip = False
+  # Walk the directory to create the zip file
+  print("Writing ZIP file: %s" % zfilename)
+  z = zipfile.ZipFile(zfilename, 'w')
+  for r, d, f in os.walk('.'):
+    for ff in f:
+      skip = False
 
-    # If it's not one of the files we're excluding
-    for ext in EXCLUDE_EXTS:
-      if ff.endswith(ext):
-        skip = True
+      # If it's not one of the files we're excluding
+      for ext in EXCLUDE_EXTS:
+        if ff.endswith(ext):
+          skip = True
 
-    # Skip any files
-    for fn in EXCLUDE_FILES:
-      if ff == fn:
-        skip = True
+      # Skip any files
+      for fn in EXCLUDE_FILES:
+        if ff == fn:
+          skip = True
 
-    # Skip any directories
-    for dr in EXCLUDE_DIRS:
-      if r.find(dr) > -1:
-        skip = True
+      # Skip any directories
+      for dr in EXCLUDE_DIRS:
+        if r.find(dr) > -1:
+          skip = True
 
-    if not skip:
-      z.write(os.path.join(r, ff), os.path.join(name, r, ff), zipfile.ZIP_DEFLATED)
-z.close()
+      if not skip:
+        z.write(os.path.join(r, ff), os.path.join(name, r, ff), zipfile.ZIP_DEFLATED)
+  z.close()

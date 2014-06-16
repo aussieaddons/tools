2e9329762e71ca4a59d379979f9eb439b72fd521
Andy Botting <andy@andybotting.com>
2014-06-16 11:24:16 +1000

    Don't do silly deps folder stuff. Just make one zip file.

--- build_xbmc_zip.py
+++ build_xbmc_zip.py
@@ -35,40 +35,9 @@ for r, d, f in os.walk('.'):
 
     # Skip any directories
     for dr in EXCLUDE_DIRS:
-      if (r.find(dr) > -1) or (r.find('deps') > -1):
+      if r.find(dr) > -1:
         skip = True
 
     if not skip: 
       z.write(os.path.join(r, ff), os.path.join(name, r, ff), zipfile.ZIP_DEFLATED)
 z.close()
-
-# Build XBMC ZIP file with all required depenencies if deps
-# directory exists
-if os.path.isdir(os.path.join('resources','lib','deps')):
-  zfilename = "%s-%s_deps.zip" % (name, version)
-
-  # Walk the directory to create the zip file
-  print("Writing ZIP file: %s" % zfilename)
-  z = zipfile.ZipFile(zfilename, 'w')
-  for r, d, f in os.walk('.'):
-    for ff in f:
-      skip = False
-
-      # If it's not one of the files we're excluding
-      for ext in EXCLUDE_EXTS:
-        if ff.endswith(ext):
-          skip = True
-
-      # Skip any files
-      for fn in EXCLUDE_FILES:
-        if ff == fn:
-          skip = True
-
-      # Skip any directories
-      for dr in EXCLUDE_DIRS:
-        if r.find(dr) > -1:
-          skip = True
-
-      if not skip:
-        z.write(os.path.join(r, ff), os.path.join(name, r, ff), zipfile.ZIP_DEFLATED)
-  z.close()
